107670e770b217aa83c575370e0364a2a7fd84c1
ykrueng <yasirah.krueng@gmail.com>
2019-01-18 10:17:31 -0600

    change database name in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./data/dev.sqlite3"
+      filename: "./data/gtd.sqlite3"
     },
     useNullAsDefault: true,
     migrations: {
