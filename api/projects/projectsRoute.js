9ab09184d04f5de162922d9ae9c87a340254f59e
ykrueng <yasirah.krueng@gmail.com>
2019-01-18 11:45:49 -0600

    add GET /projects/:id endpoint

--- api/projects/projectsRoute.js
+++ api/projects/projectsRoute.js
@@ -4,14 +4,25 @@ const db = require("../../data/helpers/projectsDB");
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
-  const { project } = req.body;
-  try {
-    const id = await db.insert(project);
-    res.status(201).json({ id: id[0] });
-  } catch (err) {
-    next({ code: 500 });
-  }
-});
+router
+  .post("/", async (req, res, next) => {
+    const { project } = req.body;
+    try {
+      const id = await db.insert(project);
+      res.status(201).json({ id: id[0] });
+    } catch (err) {
+      next({ code: 500 });
+    }
+  })
+  .get("/:id", async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const project = await db.getProject(id);
+      console.log(project)
+      res.status(200).json(project);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
--- data/helpers/projectsDB.js
+++ data/helpers/projectsDB.js
@@ -1,7 +1,34 @@
 const db = require("../config/dbConfig");
 
 module.exports = {
+
   insert: function (project) {
     return db("projects").insert(project);
   },
+
+  getProject: function (id) {
+    const project = db("projects")
+      .select(
+        "projects.id",
+        "projects.name",
+        "projects.description",
+        "flags.name as status")
+      .join("flags", "projects.flag_id", "flags.id")
+      .where("projects.id", id)
+      .first();
+
+    const actions = db("actions")
+      .select(
+        "actions.id",
+        "actions.description",
+        "actions.notes",
+        "flags.name as status")
+      .join("flags", "actions.flag_id", "flags.id")
+      .where("actions.project_id", id);
+
+    return Promise.all([project, actions]).then(results => {
+      const [project, actions] = results;
+      return {...project, actions};
+    });
+  }
 };
